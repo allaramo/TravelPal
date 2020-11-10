17865074d5b296b010be0a8139cb29914cb1c6c8
Alan Arango <allaramo@gmail.com>
2020-11-10 00:44:20 +0000

    added visual styles

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
+import { AppLoading } from 'expo';
 import * as Location from 'expo-location';
+import {
+  useFonts,
+  Ubuntu_300Light,
+  Ubuntu_400Regular,
+  Ubuntu_500Medium,
+  Ubuntu_700Bold,
+} from "@expo-google-fonts/ubuntu";
 
 export default function App() {
   const [location, setLocation] = useState({
@@ -11,6 +19,8 @@ export default function App() {
     city: "",
     currency:""
   });
+
+  const [date, setDate] = useState(new Date());
 
   const [weather, setWeather] = useState({
     description: "",
@@ -30,47 +40,54 @@ export default function App() {
     date : ""
   })
 
-  const [amountExchange, setAmountExchange] = useState(1);
+  const [amountExchange, setAmountExchange] = useState("1");
 
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestPermissionsAsync();
       if (status !== 'granted') {
         console.log('Permission to access gps was denied');
-      }
-
-      let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 });
-      let openCageUrl = "https://api.opencagedata.com/geocode/v1/json?key=44a9f29b61514c1bb30d4781d418d6f3&q=" +
-      location.coords.latitude +
-      "+" +
-      location.coords.longitude;
-      fetch(openCageUrl)
-        .then((response) => {
-          return response.json();
-        })
-        .then((json) => {          
-          let city = "";
-          if ("city_district" in json.results[0].components) {            
-            city = json.results[0].components.city_district;
-            city = city.split(" ")[0];
-          } else if ("town" in json.results[0].components) {
-            city = json.results[0].components.town;
-          } else if ("district" in json.results[0].components) {
-            city = json.results[0].components.district;
-          }          
-          setLocation({
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            country: json.results[0].components.country,
-            city: city,
-            currency: json.results[0].annotations.currency.iso_code
-          });
-          getWeather(location.coords.latitude,location.coords.longitude); 
-          let currencyParam = json.results[0].annotations.currency.iso_code;          
-          getCurrency("USD",currencyParam,1);
-        });     
+      } else {
+        getLocation(true);
+      }     
     })();
   }, []);  
+
+  async function getLocation(updateCurrency){
+    let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 });
+    let openCageUrl = "https://api.opencagedata.com/geocode/v1/json?key=44a9f29b61514c1bb30d4781d418d6f3&q=" +
+    location.coords.latitude +
+    "+" +
+    location.coords.longitude;
+    let currencyParam;
+    fetch(openCageUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {          
+      let city = "";
+      if ("city_district" in json.results[0].components) {            
+        city = json.results[0].components.city_district;
+        city = city.split(" ")[0];
+      } else if ("town" in json.results[0].components) {
+        city = json.results[0].components.town;
+      } else if ("district" in json.results[0].components) {
+        city = json.results[0].components.district;
+      }          
+      setLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        country: json.results[0].components.country,
+        city: city,
+        currency: json.results[0].annotations.currency.iso_code
+      });
+      getWeather(location.coords.latitude,location.coords.longitude); 
+      currencyParam = json.results[0].annotations.currency.iso_code; 
+      if(updateCurrency) {         
+        getCurrency("USD",currencyParam,1);  
+      }
+    });   
+  }
 
   function getWeather(latitude,longitude) {
     let weatherStackUrl = "http://api.weatherstack.com/current?access_key=59f7e5aea92d9b246584e72d35e0f13c&query=" +
@@ -79,11 +96,11 @@ export default function App() {
     .then((response)=>{
       return response.json();
     })
-    .then((json) => {      
+    .then((json) => {    
       setWeather({
         description: json.current.weather_descriptions[0],
         temperature: json.current.temperature,
-        precipitation: json.current.precip,
+        precipitation: json.current.precip*100,
         humidity: json.current.humidity,
         wind: json.current.wind_speed,
         icon: json.current.weather_icons[0]
@@ -119,39 +136,138 @@ export default function App() {
     getCurrency(currencyExchange.target,currencyExchange.base,amountExchange);
   }
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text>{location.latitude}</Text>
-      <Text>{location.longitude}</Text>
-      <Text>{location.country}</Text>
-      <Text>{location.city}</Text>
-      <Text>{location.currency}</Text>
-      <Text>{weather.description}</Text>
-      <Text>{weather.temperature}</Text>
-      <Text>{weather.precipitation}</Text>
-      <Text>{weather.humidity}</Text>
-      <Text>{weather.wind}</Text>
-      <Image source={{uri:weather.icon}} style={{ width: 100, height: 100 }}/>     
-      <Text>{currencyExchange.date}</Text>
-      <Text>{currencyExchange.rate}</Text>
-      <TextInput value={amountExchange} onChangeText={text => setAmountExchange(text)}/>
-      <Text>{currencyExchange.base}</Text>
-      <Button title="<- ->" onPress={flipCurrency}/>
-      <Text>{currencyExchange.targetValue}</Text>
-      <Text>{currencyExchange.target}</Text>
-      <Button title="Convert" onPress={getNewExchange}/>
-    </View>
-  );
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getLocation(false);
+    }, 60000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+
+  let [fontsLoaded] = useFonts({
+    Ubuntu_300Light,
+    Ubuntu_400Regular,
+    Ubuntu_500Medium,
+    Ubuntu_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (      
+      <View style={styles.container}>
+        <View style={styles.weather}>
+          <Text style={styles.title}>{location.city}, {location.country}</Text>
+          <Text style={styles.subtitle}>{date.toUTCString()}</Text>
+          <Text style={styles.subtitle}>{weather.description}</Text>
+          <Image style={styles.weatherIcon} source={{uri:weather.icon}}/>          
+          <Text style={styles.temperature}>{weather.temperature}°C</Text>
+          <Text style={styles.subtitle2}>Precipitation: {weather.precipitation}% Humidity: {weather.humidity}% Wind: {weather.wind} km/h</Text>          
+        </View>
+        <View style={styles.currency}> 
+          <View style={styles.row}>        
+            <Text style={styles.subtitle3}>{currencyExchange.base}</Text>
+            <Text style={styles.subtitle3}>{currencyExchange.target}</Text>
+          </View>
+          <View style={styles.row}>
+            <TextInput style={styles.box} value={amountExchange} onChangeText={text => setAmountExchange(text)}/>
+            <Button title="<- ->" onPress={flipCurrency}/>
+            <Text style={styles.box}>{currencyExchange.targetValue}</Text> 
+          </View>
+          <View style={styles.row}>       
+            <Button title="Convert" onPress={getNewExchange}/>             
+          </View>         
+            
+          
+        </View>
+      </View>
+    );
+  }
 }
-//<Button title="Convert" onPress={getCurrency(currencyExchange.base, currencyExchange.target, currencyExchange.baseValue)}/>
- //<TextInput onChangeText={getCurrency("USD",location.currency,1)}/>
- //<TextInput onChangeText={getCurrency(location.currency,"USD",1)}/>
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  weather: {
+    backgroundColor: '#333',
+    alignItems: 'center',
+    padding: 30,
+    borderRadius: 5,
+    width: 300,
+    marginVertical: 10,
+  },
+  weatherIcon: {    
+    height: 50,
+    width: 50,
+    margin: 10,
+    borderRadius: 50,
+  },
+  temperature: {   
+    color: '#eee',   
+    fontSize: 20,
+    fontFamily: 'Ubuntu_700Bold',    
+  },
+  currency: {
+    backgroundColor: '#159',
+    alignItems: 'center',
+    padding: 30,
+    borderRadius: 5,
+    width: 300,
+  },
+  row: {    
+    alignItems: 'center',    
+    paddingVertical: 5,    
+    flexDirection: 'row',
+  },
+  title: {
+    fontFamily: 'Ubuntu_500Medium',
+    color : '#ccc',    
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  subtitle: {
+    fontFamily: 'Ubuntu_400Regular',
+    color : '#aaa',    
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  subtitle2: {
+    fontFamily: 'Ubuntu_300Light',
+    color : '#aaa',
+    fontWeight: 'bold',
+    fontSize: 10,
+    marginTop: 20,
+  },
+  subtitle3: {
+    fontFamily: 'Ubuntu_300Light',
+    color : '#eee',
+    fontWeight: 'bold',
+    fontSize: 20,    
+    paddingHorizontal: 80,
+  }, 
+  box:{
+    borderWidth: 1,
+    borderColor: '#eee',
+    color : '#fff',
+    fontWeight: 'bold',
+    fontSize: 20,
+    width: 100,
+    padding: 5,    
+    margin: 10,
+    textAlign: 'right'
+  }
 });
